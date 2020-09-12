11a2d1117c093fc5b87f8457b561806f9a49002f
frandacos <franco_cosci@hotmail.com>
2020-09-12 18:20:27 -0300

    bugs

---
+++ solucion/src/BugTracker/Cursos/frmABMCurso.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace BugTracker.Cursos
+{
+    partial class frmABMCurso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmABMCurso));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblIdCurso = new System.Windows.Forms.Label();
+            this.dtpFechaVigencia = new System.Windows.Forms.DateTimePicker();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblFechaVigencia = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtIdCurso = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(60, 50);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 29;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblIdCurso
+            // 
+            this.lblIdCurso.AutoSize = true;
+            this.lblIdCurso.Location = new System.Drawing.Point(61, 17);
+            this.lblIdCurso.Name = "lblIdCurso";
+            this.lblIdCurso.Size = new System.Drawing.Size(46, 13);
+            this.lblIdCurso.TabIndex = 40;
+            this.lblIdCurso.Text = "Id Curso";
+            this.lblIdCurso.Click += new System.EventHandler(this.lblId_Click);
+            // 
+            // dtpFechaVigencia
+            // 
+            this.dtpFechaVigencia.Enabled = false;
+            this.dtpFechaVigencia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFechaVigencia.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaVigencia.Location = new System.Drawing.Point(116, 114);
+            this.dtpFechaVigencia.Name = "dtpFechaVigencia";
+            this.dtpFechaVigencia.Size = new System.Drawing.Size(166, 22);
+            this.dtpFechaVigencia.TabIndex = 45;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Enabled = false;
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(26, 166);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(342, 88);
+            this.txtDescripcion.TabIndex = 44;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(116, 46);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(2, 22);
+            this.textBox5.TabIndex = 43;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(40, 145);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(69, 13);
+            this.lblDescripcion.TabIndex = 42;
+            this.lblDescripcion.Text = "Descripción: ";
+            // 
+            // lblFechaVigencia
+            // 
+            this.lblFechaVigencia.AutoSize = true;
+            this.lblFechaVigencia.Location = new System.Drawing.Point(24, 119);
+            this.lblFechaVigencia.Name = "lblFechaVigencia";
+            this.lblFechaVigencia.Size = new System.Drawing.Size(83, 13);
+            this.lblFechaVigencia.TabIndex = 41;
+            this.lblFechaVigencia.Text = "Fecha vigencia:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(192, 269);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(80, 40);
+            this.btnCancelar.TabIndex = 49;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(108, 269);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(78, 40);
+            this.btnAceptar.TabIndex = 48;
+            this.btnAceptar.Text = "&Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Enabled = false;
+            this.txtCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoria.Location = new System.Drawing.Point(116, 82);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(166, 22);
+            this.txtCategoria.TabIndex = 51;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(52, 86);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(60, 13);
+            this.lblCategoria.TabIndex = 50;
+            this.lblCategoria.Text = "Categoría: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(116, 46);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(166, 22);
+            this.txtNombre.TabIndex = 52;
+            // 
+            // txtIdCurso
+            // 
+            this.txtIdCurso.Enabled = false;
+            this.txtIdCurso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdCurso.Location = new System.Drawing.Point(116, 13);
+            this.txtIdCurso.Name = "txtIdCurso";
+            this.txtIdCurso.Size = new System.Drawing.Size(45, 22);
+            this.txtIdCurso.TabIndex = 53;
+            // 
+            // frmABMCurso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 314);
+            this.Controls.Add(this.txtIdCurso);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.dtpFechaVigencia);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblFechaVigencia);
+            this.Controls.Add(this.lblIdCurso);
+            this.Controls.Add(this.lblNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmABMCurso";
+            this.Text = "Edición Curso";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblIdCurso;
+        private System.Windows.Forms.DateTimePicker dtpFechaVigencia;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblFechaVigencia;
+        internal System.Windows.Forms.Button btnCancelar;
+        internal System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtIdCurso;
+    }
+}
---
+++ solucion/src/BugTracker/GUILayer/frmConsultaBugs.designer.cs
@@ -0,0 +1,105 @@
+﻿namespace BugTracker.GUILayer
+{
+    partial class frmConsultaBugs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetalleBug = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lbl_estado = new System.Windows.Forms.Label();
+            this.cboEstados = new System.Windows.Forms.ComboBox();
+            this.pnl_filtros = new System.Windows.Forms.GroupBox();
+            this.pnl_filtros.SuspendLayout();
+            this.SuspendLayout();
+            
+           
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Image = global::BugTracker.Properties.Resources.salir;
+            this.btnSalir.Location = new System.Drawing.Point(805, 413);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(40, 40);
+            this.btnSalir.TabIndex = 30;
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lbl_estado
+            // 
+            this.lbl_estado.AutoSize = true;
+            this.lbl_estado.Location = new System.Drawing.Point(32, 51);
+            this.lbl_estado.Name = "lbl_estado";
+            this.lbl_estado.Size = new System.Drawing.Size(43, 13);
+            this.lbl_estado.TabIndex = 0;
+            this.lbl_estado.Text = "Estado:";
+            // 
+            // cboEstados
+            // 
+            this.cboEstados.FormattingEnabled = true;
+            this.cboEstados.Location = new System.Drawing.Point(76, 48);
+            this.cboEstados.Name = "cboEstados";
+            this.cboEstados.Size = new System.Drawing.Size(181, 21);
+            this.cboEstados.TabIndex = 1;
+            // 
+            // pnl_filtros
+            // 
+            this.pnl_filtros.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnl_filtros.Controls.Add(this.cboEstados);
+            this.pnl_filtros.Controls.Add(this.lbl_estado);
+            this.pnl_filtros.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_filtros.Location = new System.Drawing.Point(0, 0);
+            this.pnl_filtros.Name = "pnl_filtros";
+            this.pnl_filtros.Size = new System.Drawing.Size(852, 142);
+            this.pnl_filtros.TabIndex = 1;
+            this.pnl_filtros.TabStop = false;
+            this.pnl_filtros.Text = "Filtros";
+            // 
+            // frmConsultaBugs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 474);
+            this.Controls.Add(this.btnDetalleBug);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.pnl_filtros);
+            this.Name = "frmConsultaBugs";
+            this.Text = "Consultar Bugs";
+            this.Load += new System.EventHandler(this.frmBugs_Load);
+            this.pnl_filtros.ResumeLayout(false);
+            this.pnl_filtros.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Button btnDetalleBug;
+        internal System.Windows.Forms.Button btnSalir;
+        internal System.Windows.Forms.Label lbl_estado;
+        internal System.Windows.Forms.ComboBox cboEstados;
+        internal System.Windows.Forms.GroupBox pnl_filtros;
+    }
+}
